94d3c9b3936c307ae2d8dc6fa3bb78aca34e4c48
BlackRose01 <BlackRose01@users.noreply.github.com>
2022-03-07 23:16:37 +0100

    Change ESP8266 specific commands to ESP32; Refactor

--- pwm_fan.ino
+++ pwm_fan.ino
@@ -6,12 +6,10 @@
 
 #include <Arduino.h>
 #include <Wire.h>
-// #include <Adafruit_SHT31.h>
-#include <ESP8266WiFiMulti.h>
+#include <Adafruit_SHT31.h>
+#include <WiFi.h>
 #include <PubSubClient.h>
 #include <WiFiClient.h>
-
-#define SHT31_ADDRESS   0x44
 
 #define WIFI_SSID ""
 #define WIFI_PASS ""
@@ -25,20 +23,24 @@
 #define MAX_HUM 70
 #define MIN_HUM 40
 
-#define PWM_PIN 5
-#define RPM_PIN 4
+#define PWM_PIN 17
+#define RPM_PIN 16
 #define MIN_FAN_SPEED_PERCENT 0
+
+#define LOOP_DELAY 60
 
 bool enableHeater = false;
 int counter = 0;
 int tmp_speed = 100;
 bool tmp_speed_changed = true;
 
-ESP8266WiFiMulti wifiMulti;
 WiFiClient net;
 PubSubClient mqtt(net);
-// Adafruit_SHT31 sht31 = Adafruit_SHT31();
+Adafruit_SHT31 sht31 = Adafruit_SHT31();
 
+/**
+ * Callback for incoming MQTT messages
+ */
 void mqttCallback(char* topic, byte* payload, unsigned int length) {
   Serial.print("Message arrived [");
   Serial.print(topic);
@@ -62,6 +64,9 @@ void mqttCallback(char* topic, byte* payload, unsigned int length) {
   tmp_speed_changed = true;
 }
 
+/**
+ * Reconnect to MQTT Broker
+ */
 void reconnectMqtt() {
   // Loop until we're reconnected
   while (!mqtt.connected()) {
@@ -81,14 +86,19 @@ void reconnectMqtt() {
   mqtt.subscribe("pflanzi/vitrine/speed");
 }
 
+/**
+ * Setup
+ */
 void setup() {
+  // Open Serial
   Serial.begin(9600);
 
   // Connect WiFi
-  WiFi.mode(WIFI_STA);
-  wifiMulti.addAP(WIFI_SSID, WIFI_PASS);
-  while (wifiMulti.run() != WL_CONNECTED) {
-    delay(500);
+  WiFi.begin(WIFI_SSID, WIFI_PASS);
+  Serial.print("Connecting to WiFi ..");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print('.');
+    delay(1000);
   }
 
   // Connect MQTT
@@ -97,12 +107,19 @@ void setup() {
   reconnectMqtt();
 
   // Configure SHT31
-  // sht31.begin(SHT31_ADDRESS);
-  
+  sht31.begin();
+
+  // Configure PWM
   pinMode(PWM_PIN, OUTPUT);
   pinMode(RPM_PIN, INPUT);
+
+  ledcSetup(0, 25000, 8);
+  ledcAttachPin(PWM_PIN, 0);
 }
 
+/**
+ * Returns the speed of the fan
+ */
 int getFanSpeedRpm() {
   int highTime = pulseIn(RPM_PIN, HIGH);
   int lowTime = pulseIn(RPM_PIN, LOW);
@@ -114,15 +131,22 @@ int getFanSpeedRpm() {
   return (freq * 60.0) / 2.0; // two cycles per revolution
 }
 
+/**
+ * Set the speed of the fan
+ */
 void setFanSpeedPercent(int p) {
   int value = (p / 100.0) * 255;
-  analogWrite(PWM_PIN, value);
+  ledcWrite(0, value);
 }
 
+/**
+ * Calculate the new fan speed depending on temperature and humidity
+ */
 int calcFanSpeed(double currTemp, double currHum) {
   double percTemp = 0.0;
   double percHum = 0.0;
 
+  // Calc temperature
   if (currTemp < MIN_TEMP) {
     percTemp = 0;
   } else if (currTemp > MAX_TEMP) {
@@ -131,6 +155,7 @@ int calcFanSpeed(double currTemp, double currHum) {
     percTemp = (100 - MIN_FAN_SPEED_PERCENT) * (currTemp - MIN_TEMP) / (MAX_TEMP - MIN_TEMP) + MIN_FAN_SPEED_PERCENT;
   }
 
+  // Calc humidity
   if (currHum < MIN_HUM) {
     percHum = 0;
   } else if (currHum > MAX_HUM) {
@@ -139,9 +164,13 @@ int calcFanSpeed(double currTemp, double currHum) {
     percHum = (100 - MIN_FAN_SPEED_PERCENT) * (currHum - MIN_HUM) / (MAX_HUM - MIN_HUM) + MIN_FAN_SPEED_PERCENT;
   }
 
+  // Returns the mean value of temperature and humidity
   return ((percHum + percTemp) / 2);
 }
 
+/**
+ * Publish a MQTT message
+ */
 void mqPublish(const char * topic, String message) {
   char buf[MQTT_MAX_PACKET_SIZE];
   memset(buf, 0, sizeof(buf));
@@ -150,6 +179,9 @@ void mqPublish(const char * topic, String message) {
   mqtt.publish(topic, buf, false);
 }
 
+/**
+ * Build the JSON message
+ */
 String buildMessage(double t, double h, int speed) {
   unsigned long timestamp = 0L;
   
@@ -169,6 +201,9 @@ String buildMessage(double t, double h, int speed) {
   return data;
 }
 
+/**
+ * Scans I2C bus and output if a device is connected
+ */
 void devices() {
   byte error, address;
   int nDevices;
@@ -208,11 +243,15 @@ void devices() {
  
 void loop() {
   mqtt.loop();
-  //devices();
-  float currTemp = 0.0; // sht31.readTemperature();
-  float currHum = 0.0; // sht31.readHumidity();
+  // devices();
+
+  // Get current temperature, humidity and fan speed
+  float currTemp = sht31.readTemperature();
+  float currHum = sht31.readHumidity();
   int speed = getFanSpeedRpm() * 1.1;
   // setFanSpeedPercent(tmp_speed);
+  
+  // Publish MQTT message with current measurements
   mqPublish(MQTT_TOPIC, buildMessage(currTemp, currHum, speed));
 
   if (tmp_speed_changed) {
@@ -220,7 +259,7 @@ void loop() {
     tmp_speed_changed = false;
   }
 
-  /*
+  // Activate heater on SHT31 after 30 loops
   if (counter >= 30) {
     enableHeater = !enableHeater;
     sht31.heater(enableHeater);
@@ -228,7 +267,6 @@ void loop() {
   }
 
   counter++;
-  */
 
-  //delay(1000);
+  delay(LOOP_DELAY * 1000);
 }
